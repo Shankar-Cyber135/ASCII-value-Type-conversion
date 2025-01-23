86281469fcf7dec9ca21bacafdf23648a90d6890
Shankar Kumar Dhar <shankardhar456789@gmail.com>
2025-01-23 14:01:40 +0530

    Add files via upload

---
+++ ASCIIvalueofDandg.java
@@ -0,0 +1,11 @@
+class ASCIIvalueofDandg
+{
+    public static void main(String []args)
+    {
+        char ch1='D',ch2='g';
+        int result1=ch1;
+        int result2=ch2;
+        System.out.println("The ASCII value of D is ="+result1);
+        System.out.println("The ASCII value of g is ="+result2);
+    }
+}
